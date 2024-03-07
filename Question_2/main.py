5aad5bda586f1e6739dbff51cd5b6e4f6a974095
Matthew-Tapia <64719795+Matthew-Tapia@users.noreply.github.com>
2024-03-06 22:26:36 -0800

    Finished Question 2

---
+++ Question_2/main.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from math import sqrt
+
+
+def find_mu(data_set):
+    return sum(data_set) / len(data_set)
+
+
+def find_sigma(data_set, mu):
+    return sqrt(sum([(x_i - mu) ** 2 for x_i in data_set]) / len(data_set))
+
+
+if __name__ == "__main__":
+    data = np.loadtxt('data.txt')
+
+    # Change with own implementation
+    mean = find_mu(data)
+    stddev = find_sigma(data, mean)
+
+    print(f"Mean: {mean:.3f}")
+    print(f"Standard Deviation: {stddev:.3f}")
+
+    # Plot histogram
+    plt.hist(data, bins='auto', density=True)
+
+    # Plot PDF of the Gaussian distribution
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 100)
+    plt.plot(x, norm.pdf(x, mean, stddev))
+
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.title('Gaussian (μ, σ) PDF and Data Histogram')
+    plt.legend(['Gaussian PDF', 'Data Histogram'])
+    plt.show()
